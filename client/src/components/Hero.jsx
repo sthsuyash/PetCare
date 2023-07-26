d95441b853d98d5b90aba74a599bdcff584844a8
sthsuyash <sthasuyash11@gmail.com>
2023-07-26 23:09:17 +0545

    backend init

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
                     {/* Carousel wrapper */}
                     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
                         {/* Item 1 */}
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item="">
+                        <div className="duration-700 ease-in-out" data-carousel-item="active">
                             <img
                                 src={image}
                                 className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
