144d3febf2357ab64397af6fd24e99b3c6da62c1
sthsuyash <sthasuyash11@gmail.com>
2023-08-18 23:49:18 +0545

    backend to springboot

---
+++ src/test/java/com/suyash/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
